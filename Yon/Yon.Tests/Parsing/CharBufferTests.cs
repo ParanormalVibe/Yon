892849250c42156d7b73a7a23fd1526b4b6e8d07
Paranormal Vibe <jaray@everygame.co>
2021-03-04 17:58:23 -0500

    Add CharBuffer test

--- Yon/Yon.Tests/Parsing/CharBufferTests.cs
+++ Yon/Yon.Tests/Parsing/CharBufferTests.cs
@@ -1,7 +1,31 @@
+using System;
+using NUnit.Framework;
+using Yon.Parsing;
+
 namespace Yon.Tests.Parsing
 {
     public class CharBufferTests
     {
+        protected CharBufferSource BufferSource { get; private set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            BufferSource = new CharBufferSource();
+        }
         
+        public class ToToken : CharBufferTests
+        {
+            /// <summary>
+            /// This is expected to throw because allowing tokens with empty values
+            /// could be a problem down the road, so it's better to just avoid it.
+            /// </summary>
+            [Test]
+            public void Throws_InvalidOperationException_When_Buffer_Empty()
+            {
+                Assert.Throws<InvalidOperationException>(() =>
+                    BufferSource.Buffer.ToToken(default));
+            }
+        }
     }
 }
