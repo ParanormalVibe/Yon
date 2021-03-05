32f6fafca824092db9280ecee6221d75a1a8e2f1
Paranormal Vibe <jaray@everygame.co>
2021-03-04 20:56:59 -0500

    Add GracefulExit test

--- Yon/Yon.Tests/Parsing/GracefulExitTests.cs
+++ Yon/Yon.Tests/Parsing/GracefulExitTests.cs
@@ -1,7 +1,25 @@
+using NUnit.Framework;
+using Yon.Parsing;
+
 namespace Yon.Tests.Parsing
 {
     public class GracefulExitTests
     {
-        
+
+        public class Evaluate
+        {
+            [Test]
+            public void Creates_Delimiter_Token_When_Input_Expected_And_Buffer_Not_Empty()
+            {
+                var src = new CharBufferSource();
+                var context = new TemplateLexerContext(src.Buffer, "abcd");
+                src.Append('a');
+                src.Append('b');
+                src.Append('c');
+                src.Append('d');
+                new GracefulExit().Evaluate(context);
+                Assert.AreEqual(context.Tokens.Dequeue()?.Value, "abcd");
+            }
+        }
     }
 }
